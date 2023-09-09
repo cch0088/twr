84c439b29bcd5b4b39a4a5a5bf697814a5b64ad9
cch0088 <cons1@protonmail.ch>
2023-09-09 15:45:03 -0400

    Modal fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { closeModal } from "../features/ModalSlice";
+import { closeModal } from '../features/ModalSlice';
 import { useDispatch } from 'react-redux';
 
 function Modal({children}) 
 {
     const dispatch = useDispatch();
 
-    const handleClose = () => {
-        dispatch(closeModal());
+    const handleClose = (e) => {
+        if (e.target.className === 'modal-bg') {dispatch(closeModal());}
     };
 
     return (
-        <div className="modal" onClick={handleClose}>
+        <div className='modal-bg' onClick={(e) => handleClose(e)}>
             <section className='modal-main'>
                 {children}
             </section>
