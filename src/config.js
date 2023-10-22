ef0daa341f1b794e193e75384f1e55ac0d03d941
cch0088 <cons1@protonmail.ch>
2023-10-22 12:56:24 -0400

    New config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API = "https://www.travelwithreason.com:8080";
+export const API = "https://www.travelwithreason.com";
 export const SESSION = `${API}/session/token`;
 export const LOGIN = `${API}/user/login`;
 export const LOGOUT = `${API}/user/logout`;
